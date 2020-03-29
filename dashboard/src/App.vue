a4db6ad022418fa14ba84a7bc379c49dccab493f
Kendall Park <darkidder@gmail.com>
2020-03-29 11:20:12 -0500

    feat: add leaflet, set up initial dashboard

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,60 +1,151 @@
 <template>
-  <v-app>
-    <v-app-bar
+  <v-app id="inspire">
+    <v-navigation-drawer
+      class="above-navbar"
+      v-model="drawer"
       app
-      color="primary"
-      dark
+      temporary
+      hide-overlay
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>COVID-19 Dash</v-list-item-title>
+          </v-list-item-content>
 
-      <v-spacer></v-spacer>
+        </v-list-item>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-divider></v-divider>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      class="above-navbar"
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>COVID-19 Dashboard</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container
+        class="fill-height no-padding"
+        fluid
+      >
+        <!-- <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="shrink"> -->
+            <!-- <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  :href="source"
+                  icon
+                  large
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  href="https://codepen.io/johnjleider/pen/bXNzZL"
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-codepen</v-icon>
+                </v-btn>
+              </template>
+              <span>Codepen</span>
+            </v-tooltip> -->
+            <BaseMap/>
+            <!-- <HelloWorld/> -->
+          <!-- </v-col>
+        </v-row> -->
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020 Kendall Park</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld.vue';
+import BaseMap from './components/BaseMap.vue';
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    // HelloWorld,
+    BaseMap,
   },
-
+  props: {
+    source: String,
+  },
   data: () => ({
-    //
+    drawer: null,
   }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
 };
 </script>
+
+<style lang="scss">
+  .container.no-padding {
+    padding: 0;
+  }
+  // .above-map {
+  //   z-index: 2000 !important;
+  // }
+  // .above-navbar {
+  //   z-index: 2001 !important;
+  // }
+
+  .v-navigation-drawer--absolute {
+    z-index: 2001 !important;
+  }
+  .v-navigation-drawer--fixed {
+    z-index: 2006 !important;
+  }
+  .v-navigation-drawer--clipped
+  :not(.v-navigation-drawer--temporary)
+  :not(.v-navigation-drawer--is-mobile) {
+    z-index: 2004 !important;
+  }
+  .v-navigation-drawer--temporary {
+    z-index: 2007 !important;
+  }
+  .v-navigation-drawer--mobile {
+    z-index: 2006 !important;
+  }
+
+</style>
---
+++ dashboard/src/components/BaseMap.vue
@@ -0,0 +1,51 @@
+<template>
+  <l-map
+    :zoom="zoom"
+    :center="center"
+    @update:zoom="zoomUpdated"
+    @update:center="centerUpdated"
+    @update:bounds="boundsUpdated"
+  >
+    <l-tile-layer :url="url"></l-tile-layer>
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer } from 'vue2-leaflet';
+
+export default {
+  name: 'MyAwesomeMap',
+  components: {
+    LMap,
+    LTileLayer,
+    // LMarker,
+  },
+  data() {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 5,
+      center: [39.833333, -98.583333],
+      bounds: null,
+      // maxBounds: [[90, 180], [-90, -180]],
+      // minZoom: 2,
+      // maxBoundsViscosity: 1,
+    };
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    },
+  },
+  // mounted() {
+  //   this.$nextTick(() => {
+  //     this.$refs.myMap.mapObject.ANY_LEAFLET_MAP_METHOD();
+  //   });
+  // },
+};
+</script>
